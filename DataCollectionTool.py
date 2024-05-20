eb3684b18c6386d1105d2aa0b358723bbe50437a
GFuks27750 <s27750@pjwstk.edu.pl>
2024-05-20 23:12:30 +0200

    Working application, without some breakpoints

--- DataCollectionTool.py
+++ DataCollectionTool.py
@@ -1,84 +1,91 @@
-# TODO
-# Użytkownik w trakcie działania programu powinien móc wielokrotnie wybierać to ile zdjęć chce wykonań
-# Fajnie by było jakby na ekranie wyświetlał się jakiś feedback, że zdjęcie zostało wykonane/ile pozostało
-# do wykonania.
-# Potrzebna jest jakaś wyraźna przerwa, albokomunikat żeby przygotować się do kolejnego zdjęci, żeby foty nie
-# wychodziły rozmazane, bo jak na razie może ci w ruchu zrobić zdjęcie. (Będzie trzeba mniej czasu poświęcić na
-# oczyszczanie danych)
+#TODO 
+#Zabezpieczyć aplikacje na sytuacje wyjątkowe: 
+# 1. wpisanie litery zamiast liczby
+# 2. pisanie liczby zamiast litery
+# 3. Po wpisaniu litery powinna być automatycznie zamieniana na dużą litere
+# 4. Powinno się dać wpisać tylko jedną literę
 
 import cv2
 import time
 import threading
 import os
 
-
-def capture_image(directory, img_name, frame, remaining_images):
+def capture_image(directory, img_name, frame, remaining_images, exit_flag):
+    if exit_flag.is_set():
+        return
     if not os.path.exists(directory):
         os.makedirs(directory)
     full_path = os.path.join(directory, img_name)
     cv2.imwrite(full_path, frame)
     print(f"{full_path} written! Remaining images: {remaining_images}")
 
-
-def take_pictures(how_many_img, cam, directory, img_prefix):
+def take_pictures(how_many_img, cam, directory, img_prefix, exit_flag):
     global img_counter, remaining_images, taking_pictures
     remaining_images = how_many_img
     for _ in range(how_many_img):
+        if exit_flag.is_set():
+            break
         ret, frame = cam.read()
         if ret:
-            img_name = f"{img_prefix}_opencv_frame_{img_counter}.png"
-            threading.Thread(target=capture_image, args=(directory, img_name, frame.copy(), remaining_images - 1)).start()
+            img_name = f"{img_prefix}_{img_counter}.png"
+            threading.Thread(target=capture_image, args=(directory, img_name, frame.copy(), remaining_images - 1, exit_flag)).start()
             img_counter += 1
             remaining_images -= 1
             time.sleep(1)
         else:
             print("failed to grab frame")
     taking_pictures = False
+    exit_flag.set()
 
-
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("test")
-
-global img_counter, remaining_images, taking_pictures
-img_counter = 0
-remaining_images = 0
-taking_pictures = False
-
-while True:
-    how_many_img = int(input("How many pictures do you want to take?\n"))
-    directory = input("Enter the directory name (single letter) where images will be saved:\n")
-    img_prefix = directory
-
+def main():
+    global img_counter, remaining_images, taking_pictures
+    img_counter = 0
+    remaining_images = 0
     taking_pictures = False
+    exit_flag = threading.Event()
+
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("test")
+
     while True:
-        ret, frame = cam.read()
-        if not ret:
-            print("failed to grab frame")
+        if not taking_pictures:
+            how_many_img = int(input("How many pictures do you want to take?\n"))
+            directory = input("Enter the directory name (single letter) where images will be saved:\n")
+            img_prefix = directory
+            print("Press spacebar to start taking pictures")
+            taking_pictures = False
+
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("failed to grab frame")
+                break
+
+            if taking_pictures:
+                feedback_text = f"Taking picture... Remaining: {remaining_images}"
+            else:
+                feedback_text = "Press spacebar to start taking pictures"
+
+            cv2.putText(frame, feedback_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow("test", frame)
+
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                exit_flag.set()
+                break
+            if k % 256 == 32 and not taking_pictures:
+                # SPACE pressed
+                taking_pictures = True
+                threading.Thread(target=take_pictures, args=(how_many_img, cam, directory, img_prefix, exit_flag)).start()
+                break
+
+        if exit_flag.is_set():
             break
 
-        if taking_pictures:
-            feedback_text = f"Taking picture... Remaining: {remaining_images}"
-        else:
-            feedback_text = "Press SPACE to start taking pictures"
+    cam.release()
+    cv2.destroyAllWindows()
 
-        cv2.putText(frame, feedback_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.imshow("test", frame)
-
-        k = cv2.waitKey(1)
-        if k % 256 == 27:
-            # ESC pressed
-            print("Escape hit, closing...")
-            cam.release()
-            cv2.destroyAllWindows()
-            exit()  # Exit the program
-        elif k % 256 == 32 and not taking_pictures:
-            # SPACE pressed
-            taking_pictures = True
-            threading.Thread(target=take_pictures, args=(how_many_img, cam, directory, img_prefix)).start()
-
-        if not taking_pictures and remaining_images == 0:
-            break
-
-cam.release()
-cv2.destroyAllWindows()
-
+if __name__ == "__main__":
+    main()
